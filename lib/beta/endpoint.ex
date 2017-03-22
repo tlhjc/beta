d3f0ba37440d08e24e3bb8f55b34f8254e083640
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2017-03-22 16:39:28 +0000

    adds initial layout of the feed

--- lib/beta/endpoint.ex
+++ lib/beta/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Beta.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :beta, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt fonts)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,43 @@ defmodule Beta.PageController do
   use Beta.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    IO.puts get_date()
+
+    render conn, "index.html", date: get_date()
+  end
+
+  def get_date() do
+    {{year, month, day}, {hour, minute, second}} = :calendar.local_time()
+    date = Date.utc_today()
+    months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ]
+    days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ]
+    current_month_date = Integer.to_string(day)
+    current_day_of_week = Enum.at(days, Date.day_of_week(date) - 1)
+    current_month = Enum.at(months, month - 1)
+    current_year = Integer.to_string(year)
+
+    date = current_day_of_week <> ", " <> current_month <> " " <> current_month_date <> ", " <> current_year
+    upper_case_date = String.upcase(date)
   end
 end
